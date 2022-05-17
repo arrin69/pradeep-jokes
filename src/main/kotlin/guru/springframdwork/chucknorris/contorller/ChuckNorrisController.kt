2c38482c63ca83d42b6558fad6b71b8ebd92f180
arrin69 <pradeepbhardwaj@live.com>
2022-05-17 23:43:14 +0200

    updated with review

--- src/main/kotlin/guru/springframdwork/chucknorris/contorller/ChuckNorrisController.kt
+++ src/main/kotlin/guru/springframdwork/chucknorris/contorller/ChuckNorrisController.kt
@@ -3,7 +3,6 @@ package guru.springframdwork.chucknorris.contorller
 import guru.springframework.norris.chuck.ChuckNorrisQuotes
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Service
 import org.springframework.ui.Model
@@ -12,22 +11,29 @@ import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class ChuckNorrisController(
-    private val jokeClient: JokeClient
+    private val jokeServiceImpl: JokeServiceImpl
 ) {
     private val log: Logger = LoggerFactory.getLogger(ChuckNorrisController::class.java)
 
     @GetMapping("/joke")
     fun getNewJoke(model: Model): String {
-        val newJoke = this.jokeClient.getNewJoke()
+        val newJoke = this.jokeServiceImpl.getNewJoke()
         log.info("joke found: $newJoke")
         model.addAttribute("joker", newJoke)
         return "joker/norris"
     }
 }
 
+
+interface JokerService {
+    fun getNewJoke(): String
+}
+
 @Service
-class JokeClient {
-    fun getNewJoke(): String = ChuckNorrisQuotes()
+class JokeServiceImpl : JokerService {
+    private val chuckNorrisQuotes: ChuckNorrisQuotes = ChuckNorrisQuotes()
+
+    override fun getNewJoke(): String = this.chuckNorrisQuotes
         .randomQuote
         .replace("Chuck", "Pradeep", true)
         .replace("Norris", "Bhardwaj", true)
